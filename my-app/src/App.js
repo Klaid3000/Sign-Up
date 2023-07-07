5af235a8c2a53fe45fb57d47e9c5ed161d994e19
Yury Chaikouski <klaidik92@mail.ru>
2023-07-07 09:41:22 +0200

    without JSX

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import { createElement } from 'react';
 
 export const App = () => {
 	const dateNow = new Date(); // отображаем дату и вызываем целый год под ссылкой (это императивный стиль написания кода)
 
-	//тут return начинается декларативный стиль. Он не затрагивает элементы. Он просто объявляет, что элементы должны отображаться с учетом нашего текущего состояния.
+	/*//тут return начинается декларативный стиль. Он не затрагивает элементы. Он просто объявляет, что элементы должны отображаться с учетом нашего текущего состояния.
 	return (
 		<div className="App">
 			<header className="App-header">
@@ -23,5 +24,33 @@ export const App = () => {
 				<p>{dateNow.getFullYear()}</p>
 			</header>
 		</div>
+	);*/
+	// Код без использования JSX
+	return createElement(
+		'div',
+		{ className: 'App' },
+		createElement(
+			'header',
+			{ className: 'App-header' },
+			createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
+			createElement(
+				'p',
+				{},
+				'Edit ',
+				<code>src/App.js</code>,
+				' and save to reload.',
+			),
+			createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			createElement('p', {}, dateNow.getFullYear()),
+		),
 	);
 };
